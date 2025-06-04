8ead91c61c392459b7122c91f5a282417fc7ad22
Abhilash Dwibedi <you@example.com>
2025-06-04 22:44:33 +0530

    initial run

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cyderes-tfstate-bucket"
+    bucket         = "cyderes-tf-state"
     key            = "terraform/state"
     region         = "us-west-2"
     encrypt        = true
